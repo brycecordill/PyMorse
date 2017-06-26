6459bd6fb83a55f116ac86d311c92fd1a1bb6540
Bryce Cordill <brycecordill@gmail.com>
2017-06-25 19:36:48 -0700

    Make more functional and text to morse works

--- Morse.py
+++ Morse.py
@@ -1,5 +1,3 @@
-# import time
-
 Morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
               'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
               'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
@@ -13,32 +11,45 @@ def convert_single(letter):
     if len(letter) > 1 and ('.' not in letter and '-' not in letter):
         print("Not a single char!  Converting...")
         letter = letter[0:1]
-    if letter in Morse:
+    if letter in Morse:  # if it's a letter
         return Morse[letter]
-    elif letter in Morse.values():
+    elif letter in Morse.values():  # if it's morse
         return list(Morse.keys())[list(Morse.values()).index(letter)]
     else:
         print("Unable to match!")
         return ''
 
 
-def convert_all(string):
+def check_morse(string):
     string = str(string).upper()
-    # conversion = []
-    converted = ""
-    for c in string:
-        if c == ' ':
-            converted = converted + '   '
+    mCode = True
 
-        elif c in Morse:
-            #Only reads one letter at a time (fix that)
-            converted = converted + Morse.get(c) + " "
-        elif c in Morse.values():  # if a code char
-            converted = converted + list(Morse.keys())[list(Morse.values()).index(c)]
+    for ch in list(string):
+        # checks if in morse code, false if not
+        if not(ch is '.' or ch is '-' or ch is ' '):
+            mCode = False
+    if mCode:
+        return convert_to_text(string)
+    else:
+        return convert_to_morse(string)
+
+
+def convert_to_morse(string):
+    converted = ""
+    for ch in list(string):
+        if ch in Morse:
+            converted += Morse.get(ch) + " "
+        elif ch == " ":
+            converted += "   "
         else:
-            converted = converted + "(?) "
+            converted += "(?)"
+    return converted
+
+
+def convert_to_text(string):
+    converted = ""
     return converted
 
 
 # print('\n'+convert_single(input("Please enter a character to convert: ")))
-print('\n\n\n'+convert_all(input("Enter a phrase to convert: ")))
+print('\n\n\n'+check_morse(input("Enter a phrase to convert: ")))
